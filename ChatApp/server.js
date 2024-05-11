2708a61a89bf7e5a99c50c163d84e1e2aca5fafb
fatmaelhot <fatmaelhot289@gmail.com>
2024-05-12 00:31:57 +0300

    Lab 2

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -11,26 +11,25 @@ const connectedClients = new Map();
 
 wss.on('connection', function connection(ws) {
   let userName;
+  let nameSet = false; // Flag to track whether the user's name has been set
 
   ws.on('message', function incoming(data) {
-    const message_content = JSON.parse(data);
-    if (!userName) {
-      userName = message_content.sender;
+    if (!nameSet) {
+      userName = data;
       connectedClients.set(ws, userName);
       broadcast(`${userName} connected. Total users: ${connectedClients.size}`);
-    } else {
-      wss.clients.forEach(function each(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(data);
-        }
-      });
+      nameSet = true; // Set the flag to true after the user's name is set
+    } else if (!data.includes('connected')) {
+      broadcast(`${userName}: ${data}`);
     }
   });
 
   ws.on('close', function() {
-    const disconnectedUsername = connectedClients.get(ws);
-    connectedClients.delete(ws);
-    broadcast(`${disconnectedUsername} disconnected. Total users: ${connectedClients.size}`);
+    if (userName) {
+      const disconnectedUsername = connectedClients.get(ws);
+      connectedClients.delete(ws);
+      broadcast(`${disconnectedUsername} disconnected. Total users: ${connectedClients.size}`);
+    }
   });
 });
 
